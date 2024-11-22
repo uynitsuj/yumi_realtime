15341caa56a74b5892d4a2f5aec1c6a76806ebef
Justin Yu <yujustin8@gmail.com>
2024-11-21 18:18:27 -0800

    ptorch tensor buffer -> deque

--- yumi_realtime/policy_controller/diffusion_policy_controller.py
+++ yumi_realtime/policy_controller/diffusion_policy_controller.py
@@ -2,20 +2,18 @@ from yumi_realtime.controller import YuMiROSInterface
 from loguru import logger
 import numpy as onp
 import tyro
-from typing import Literal
-
+from typing import Tuple
 import rospy
-
 from yumi_realtime.data_logging.data_collector import DataCollector
 from yumi_realtime.base import YuMiBaseInterface
-from dp_gs.policy.model import DiffusionPolicy
+from yumi_realtime.policy_controller.utils.utils import *
 from dp_gs.policy.diffusion_wrapper import DiffusionPolicyWrapper, normalize, unnormalize
+from geometry_msgs.msg import Transform
 from cv_bridge import CvBridge
 from sensor_msgs.msg import Image, JointState
-from std_msgs.msg import Float64MultiArray, Header, String, Float64
-from std_srvs.srv import Empty, EmptyResponse
-
+from collections import deque
 import torch
+from scipy.spatial.transform import Rotation
 
 class YuMiDiffusionPolicyController(YuMiROSInterface):
     """YuMi controller for diffusion policy control."""
@@ -32,7 +30,11 @@ class YuMiDiffusionPolicyController(YuMiROSInterface):
         self.width = None
         self.image_sub = rospy.Subscriber('/camera/image_raw', Image, self.image_callback)
         
-        self.observation_history = []
+        self.proprio_buffer = deque([],maxlen=self.model.obs_horizon)
+        self.image_primary, self.image_wrist = deque([],maxlen=self.model.obs_horizon), deque([],maxlen=self.model.obs_horizon)
+        self.action_queue = deque([],maxlen=self.model.action_horizon)
+        self.prev_action = deque([],maxlen=self.model.obs_horizon)
+        self.cur_proprio = None
         
         self.bridge = CvBridge()
         
@@ -47,24 +49,68 @@ class YuMiDiffusionPolicyController(YuMiROSInterface):
             rospy.sleep(5)
             rate.sleep() # Wait for first inputs to arrive
         
-        assert type(self.observation_history) is torch.Tensor
-        
         while not rospy.is_shutdown():
-            
             input = {
-            "observation": self.observation_history,
-            "proprio": self.state # TODO: Update with proprioception data format from self.cartesian_pose_L and self.cartesian_pose_R
+            "observation": onp.array(self.image_primary),
+            "proprio": onp.array(self.proprio_buffer)
                 }
         
             action_prediction = self.model.forward(input) # Denoise action prediction from obs and proprio...
             
-            import pdb; pdb.set_trace() # TODO: Convert denoised action_prediction format to commands for YuMi
+            action = convert_abs_action(action_prediction[None],self.cur_proprio[None,None])[0] # action_horizon, action_dim
+            # temporal emsemble start
+            new_actions = deque(action[:self.model.action_horizon])
+            self.action_queue.append(new_actions)
+            actions_current_timestep = onp.empty((len(self.action_queue), self.model.action_dim))
+            
+            k = 0.05
+            for i, q in enumerate(self.action_queue):
+                actions_current_timestep[i] = q.popleft()
+            exp_weights = onp.exp(k * onp.arange(actions_current_timestep.shape[0]))
+            exp_weights = exp_weights / exp_weights.sum()
+            action = (actions_current_timestep * exp_weights[:, None]).sum(axis=0)
+            
+            # TODO: Convert denoised action_prediction format to commands for YuMi
+            ######################################################################
+            l_xyz, l_wxyz = None, None
+            r_xyz, r_wxyz = None, None
+            
+            super().update_target_pose(
+            side='left',
+            position=l_xyz,
+            wxyz=l_wxyz,
+            gripper_state=False, # Binary
+            enable=True
+            )
+            
+            super().update_target_pose(
+            side='right',
+            position=r_xyz,
+            wxyz=r_wxyz,
+            gripper_state=False, # Binary
+            enable=True
+            )
+            ######################################################################
             
             YuMiBaseInterface.solve_ik()
             YuMiBaseInterface.update_visualization()
             super().publish_joint_commands()
-                
+            
             rate.sleep()
+    
+    def update_curr_proprio(self):
+        l_xyz, l_xyzw = tf2xyz_quat(self.cartesian_pose_L)
+        r_xyz, r_xyzw = tf2xyz_quat(self.cartesian_pose_R)
+        
+        l_q = Rotation.from_quat(l_xyzw)
+        l_rot = l_q.as_matrix()
+        l_rot_6d = rot_mat_to_rot_6d(l_rot) # [N, 6]
+        r_q = Rotation.from_quat(r_xyzw)
+        r_rot = r_q.as_matrix()
+        r_rot_6d = rot_mat_to_rot_6d(r_rot) # [N, 6]
+        
+        self.cur_proprio = onp.concatenate([l_xyz, l_rot_6d, int(self.gripper_L_pos.value)/10000, r_xyz, r_rot_6d, int(self.gripper_R_pos.value)/10000], axis=-1)
+        self.proprio_buffer.append(self.cur_proprio)
         
     def image_callback(self, image_msg: Image):
         """Handle camera observation updates."""
@@ -73,18 +119,14 @@ class YuMiDiffusionPolicyController(YuMiROSInterface):
             self.width = image_msg.width
             logger.info(f"First image received; Observation dim: {self.height}x{self.width}x3")
             
-            # Reinitialize observation history with correct dimensions
-            self.observation_history = torch.zeros(
-                (self.model.obs_horizon, self.height, self.width, 3), dtype=torch.float32
-            )
-            
-        np_img = self.bridge.imgmsg_to_cv2(image_msg, desired_encoding='rgb8').astype("float32") / 255.0  # Normalized to float [0, 1]
+        #todo: center crop image or padding    
+        image_msg  = image_msg.resize((224, 224), Image.Resampling.LANCZOS)
         
-        # Convert NumPy array to PyTorch tensor and add to history
-        new_obs = torch.from_numpy(np_img).permute(2, 0, 1)  # Convert HWC to CHW format (PyTorch standard)
-        self.observation_history = torch.cat(
-            (self.observation_history[1:], new_obs.unsqueeze(0)), dim=0
-        )  # Maintain fixed-size queue
+        onp_img = self.bridge.imgmsg_to_cv2(image_msg, desired_encoding='rgb8').astype("float32") / 255.0  # Normalized to float [0, 1]
+        
+        new_obs = onp_img.permute(2, 0, 1)
+        self.image_primary.append(new_obs)
+        self.update_curr_proprio()
 
     def _control_l(self, data):
         """Handle left controller updates."""
@@ -129,6 +171,17 @@ class YuMiDiffusionPolicyController(YuMiROSInterface):
             gripper_state=data.target_gripper_pos,
             enable=data.enable
         )
+        
+    def start_episode(self):
+        """Reset the environment and start a new episode."""
+        self.image_primary.clear()
+        self.image_wrist.clear()
+        self.prev_action.clear()
+        self.proprio_buffer.clear()
+        self.action_queue.clear()
+        self.cur_proprio = None
+        self.home() # Move to home position as first action
+        rospy.sleep(5)
         
 def main(): 
     yumi_interface = YuMiDiffusionPolicyController()
---
+++ yumi_realtime/policy_controller/utils/utils.py
@@ -0,0 +1,98 @@
+from yumi_realtime.controller import YuMiROSInterface
+from loguru import logger
+import numpy as onp
+import tyro
+from typing import Tuple
+
+
+from yumi_realtime.data_logging.data_collector import DataCollector
+from yumi_realtime.base import YuMiBaseInterface
+from dp_gs.policy.diffusion_wrapper import DiffusioonpolicyWrapper, normalize, unnormalize
+from geometry_msgs.msg import Transform
+from cv_bridge import CvBridge
+from sensor_msgs.msg import Image, JointState
+from collections import deque
+import torch
+from scipy.spatial.transform import Rotation
+
+def gram_schmidt(vectors : onp.ndarray) -> onp.ndarray: 
+    """
+    Apply Gram-Schmidt process to a set of vectors
+    vectors are indexed by rows 
+
+    vectors: batchsize, N, D 
+
+    return: batchsize, N, D
+    """
+    if len(vectors.shape) == 2:
+        vectors = vectors[None]
+    
+    basis = onp.zeros_like(vectors)
+    basis[:, 0] = vectors[:, 0] / onp.linalg.norm(vectors[:, 0], axis=-1, keepdims=True)
+    for i in range(1, vectors.shape[1]):
+        v = vectors[:, i]
+        for j in range(i):
+            v -= onp.sum(v * basis[:, j], axis=-1, keepdims=True) * basis[:, j]
+        basis[:, i] = v / onp.linalg.norm(v, axis=-1, keepdims=True)
+    return basis
+
+def rot_6d_to_rot_mat(rot_6d : onp.ndarray) -> onp.ndarray:
+    """
+    Convert a 6d representation to rotation matrix
+    rot_6d: N, 6
+
+    return: N, 3, 3
+    """
+    rot_6d = rot_6d.reshape(-1, 2, 3)
+    # assert the first two vectors are orthogonal
+    if not onp.allclose(onp.sum(rot_6d[:, 0] * rot_6d[:, 1], axis=-1), 0):
+        rot_6d = gram_schmidt(rot_6d)
+
+    rot_mat = onp.zeros((rot_6d.shape[0], 3, 3))
+    rot_mat[:, :2, :] = rot_6d
+    rot_mat[:, 2, :] = onp.cross(rot_6d[:, 0], rot_6d[:, 1])
+    return rot_mat
+
+def rot_mat_to_rot_6d(rot_mat : onp.ndarray) -> onp.ndarray: 
+    """
+    Convert a rotation matrix to 6d representation
+    rot_mat: N, 3, 3
+
+    return: N, 6
+    """
+    rot_6d = rot_mat[:, :2, :] # N, 2, 3
+    return rot_6d.reshape(-1, 6) # N, 6
+
+
+def convert_abs_action(action,proprio):
+    '''
+    Calculate the next state from the delta action and the current proprioception
+    action: S, T, action_dim
+    proprio: S, T, proprio_dim
+    '''
+    delta_trans = action[:, :, :3].reshape(-1, 3)
+    delta_rot = action[:, :, 3:9].reshape(-1,6)
+    delta_rot =  Rotation.from_matrix(rot_6d_to_rot_mat(delta_rot))
+    
+    current_state = onp.repeat(proprio[:, 0:1],action.shape[1],1)
+    current_trans = current_state[:, :, :3].reshape(-1, 3)
+    current_rot = Rotation.from_matrix(rot_6d_to_rot_mat(current_state[:,:, 3:9].reshape(-1,6)))
+    
+    trans = onp.einsum('ijk,ik->ij',current_rot.as_matrix(),delta_trans) + current_trans
+    rot = (current_rot*delta_rot).as_matrix()
+    
+    rot = rot_mat_to_rot_6d(rot).reshape(-1,action.shape[1],6)
+    trans = trans.reshape(-1,action.shape[1],3)
+    
+    if action.shape[-1] == proprio.shape[-1]:
+        #no eos
+        desired_mat = onp.concatenate([trans, rot, action[:,:,-1:]], axis=-1)
+    else:
+        #with eos
+        desired_mat = onp.concatenate([trans, rot, action[:,:,-2:]], axis=-1)
+    return desired_mat
+
+def tf2xyz_quat(tf: Transform) -> Tuple[onp.ndarray, onp.ndarray]:
+    """Convert ROS Transform message to XYZ and Quaternion XYZW."""
+    return onp.array([tf.translation.x, tf.translation.y, tf.translation.z]), onp.array([tf.rotation.x, tf.rotation.y, tf.rotation.z, tf.rotation.w])
+        
